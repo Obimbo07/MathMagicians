31c6da86fa179bbdfca914b5706b854c25bf325a
Obimbo07 <obimboausts@gmail.com>
2023-10-20 00:26:18 +0300

    calculator-events

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,139 +1,149 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 import Result from './Result';
 
 const Calculator = () => {
-  const handleClick = (buttonName) => (`${buttonName}clicked`);
+  const [value, setValue] = useState({
+    total: '0',
+    next: null,
+    operation: null,
+  });
+  const { total, operation, next } = value;
+
+  function clickHandler(num) {
+    setValue(calculate(value, num));
+  }
   return (
     <div className="math-container">
       <div className="container">
         <h2 className="heading">Lets do some Math</h2>
         <section className="section">
           <div className="results gray-bg flex-container">
-            <Result />
+            <Result result={next || operation || total || '0'} />
           </div>
           <button
             type="button"
             className="items item1 light-bg"
-            onClick={() => handleClick('AC')}
+            onClick={(e) => clickHandler(e.target.textContent)}
           >
             AC
           </button>
           <button
             type="button"
             className="items item2 light-bg"
-            onClick={() => handleClick('+/-')}
+            onClick={(e) => clickHandler(e.target.textContent)}
           >
             +/-
           </button>
           <button
             type="button"
             className="items item3 light-bg"
-            onClick={() => handleClick('%')}
+            onClick={(e) => clickHandler(e.target.textContent)}
           >
             %
           </button>
           <button
             type="button"
             className="items item4 golden-bg"
-            onClick={() => handleClick('/')}
+            onClick={(e) => clickHandler(e.target.textContent)}
           >
             /
           </button>
           <button
             type="button"
             className="items item5 light-bg"
-            onClick={() => handleClick('7')}
+            onClick={(e) => clickHandler(e.target.textContent)}
           >
             7
           </button>
           <button
             type="button"
             className="items item6 light-bg"
-            onClick={() => handleClick('8')}
+            onClick={(e) => clickHandler(e.target.textContent)}
           >
             8
           </button>
           <button
             type="button"
             className="items item7 light-bg"
-            onClick={() => handleClick('9')}
+            onClick={(e) => clickHandler(e.target.textContent)}
           >
             9
           </button>
           <button
             type="button"
             className="items item8 golden-bg"
-            onClick={() => handleClick('x')}
+            onClick={(e) => clickHandler(e.target.textContent)}
           >
             x
           </button>
           <button
             type="button"
             className="items item9 light-bg"
-            onClick={() => handleClick('4')}
+            onClick={(e) => clickHandler(e.target.textContent)}
           >
             4
           </button>
           <button
             type="button"
             className="items item10 light-bg"
-            onClick={() => handleClick('5')}
+            onClick={(e) => clickHandler(e.target.textContent)}
           >
             5
           </button>
           <button
             type="button"
             className="items item11 light-bg"
-            onClick={() => handleClick('6')}
+            onClick={(e) => clickHandler(e.target.textContent)}
           >
             6
           </button>
           <button
             type="button"
             className="items item12 golden-bg"
-            onClick={() => handleClick('+')}
+            onClick={(e) => clickHandler(e.target.textContent)}
           >
             +
           </button>
           <button
             type="button"
             className="items item13 light-bg"
-            onClick={() => handleClick('1')}
+            onClick={(e) => clickHandler(e.target.textContent)}
           >
             1
           </button>
           <button
             type="button"
             className="items item14 light-bg"
-            onClick={() => handleClick('2')}
+            onClick={(e) => clickHandler(e.target.textContent)}
           >
             2
           </button>
           <button
             type="button"
             className="items item15 light-bg"
-            onClick={() => handleClick('3')}
+            onClick={(e) => clickHandler(e.target.textContent)}
           >
             3
           </button>
           <button
             type="button"
             className="items item16 golden-bg"
-            onClick={() => handleClick('-')}
+            onClick={(e) => clickHandler(e.target.textContent)}
           >
             -
           </button>
           <button
             type="button"
             className="items item17 light-bg"
-            onClick={() => handleClick('0')}
+            onClick={(e) => clickHandler(e.target.textContent)}
           >
             0
           </button>
           <button
             type="button"
             className="items item18 golden-bg"
-            onClick={() => handleClick('=')}
+            onClick={(e) => clickHandler(e.target.textContent)}
           >
             =
           </button>
