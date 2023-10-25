ed181dc44be833b12b19464a0a755b11ac27dafa
Obimbo07 <obimboausts@gmail.com>
2023-10-25 17:41:00 +0300

    fixed-failed-test

--- src/Result.test.js
+++ src/Result.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Result from './components/Result';
+import Results from './components/Result';
 
 describe('Results component', () => {
   it('renders correctly with a number value', () => {
-    const { asFragment } = render(<Result value={42} />);
+    const { asFragment } = render(<Results value="42" />);
     expect(asFragment()).toMatchSnapshot();
   });
 
   it('renders correctly with a different number value', () => {
-    const { asFragment } = render(<Result value={123} />);
+    const { asFragment } = render(<Results value="123" />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
